ae6268dbb6b3f5bccbf4187c9c9630037121da98
hall-e3 <hallenochasanda@gmail.com>
2023-10-06 13:15:49 +0300

    new images

---
+++ src/constants/originals.jsx
@@ -0,0 +1,35 @@
+const theo_white_logo = require("../assets/orignals/Theo_white_logo.png");
+const theo_white_blue_logo = require("../assets/orignals/Theo_white_blue_logo.png");
+const theo_white_orange_logo = require("../assets/orignals/Theo_white_orange_logo.png");
+const theo_blue_orange_logo = require("../assets/orignals/Theo_blue_orange_logo.png");
+const trackie = require("../assets/orignals/trackie.jpeg");
+const trackie1 = require("../assets/orignals/trackie1.jpeg");
+const back = require("../assets/orignals/back.jpeg");
+const storage = require("../assets/orignals/storage.jpeg");
+const staff = require("../assets/orignals/staff.jpeg");
+const staff1 = require("../assets/orignals/staff1.jpeg");
+const staff2 = require("../assets/orignals/staff2.jpeg");
+const staff3 = require("../assets/orignals/staff3.jpeg");
+const background = require("../assets/orignals/background.jpeg");
+const background1 = require("../assets/orignals/background1.jpeg");
+const background2 = require("../assets/orignals/background2.jpeg");
+const background3 = require("../assets/orignals/background3.jpeg");
+
+export {
+  theo_white_logo,
+  trackie,
+  trackie1,
+  staff,
+  staff1,
+  staff2,
+  staff3,
+  storage,
+  back,
+  background,
+  background1,
+  background2,
+  background3,
+  theo_blue_orange_logo,
+  theo_white_blue_logo,
+  theo_white_orange_logo,
+};
